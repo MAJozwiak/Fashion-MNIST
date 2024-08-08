783249d0b3fc82f110ab886673e0ad80e8c04abe
Marta <martajjoo9@gmail.com>
2024-08-08 21:42:33 +0200

    Create dataset and dataloader function

---
+++ src/dataset_dataloader/dataset_dataloader.py
@@ -0,0 +1,17 @@
+import torchvision
+import torchvision.transforms as transforms
+import torch
+import torch.utils.data as data
+
+def dataset_dataloader(data_root):
+    transform = transforms.Compose(
+        [transforms.ToTensor(),])
+
+    train_dataset = torchvision.datasets.FashionMNIST(root=data_root, train=True, download=True, transform=transform)
+    test_dataset = torchvision.datasets.FashionMNIST(root=data_root, train=False, download=True, transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=64, shuffle=True, num_workers=4)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False, num_workers=4)
+    print("Test data shape:", test_dataset.data.shape)
+    print("Train data shape:", train_dataset.data.shape)
+    return(train_loader, test_loader)
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from dataset_dataloader import dataset_dataloader
+
+
+data_root = r"C:\Users\marta\PycharmProjects\second\Fasion-MNIST\data"
+dataset_dataloader.dataset_dataloader(data_root)
